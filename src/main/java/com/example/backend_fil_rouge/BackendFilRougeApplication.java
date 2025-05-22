f527af376ed463457ec416db57fc3eb1e02bea22
p-chd <p.chandieu@gmail.com>
2025-05-22 14:08:52 +0200

    Initial commit

---
+++ src/main/java/com/example/backend_fil_rouge/BackendFilRougeApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_fil_rouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFilRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFilRougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_fil_rouge/BackendFilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_fil_rouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
