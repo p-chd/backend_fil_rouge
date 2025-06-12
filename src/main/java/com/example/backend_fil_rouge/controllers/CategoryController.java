033ee3df63a05901a438fc31f74d7525a6b90a76
p-chd <p.chandieu@gmail.com>
2025-06-12 13:54:24 +0200

    Update CategoryController.java
    Update: @RestController to @Controller to try implementing views later

--- src/main/java/com/example/backend_fil_rouge/controllers/CategoryController.java
+++ src/main/java/com/example/backend_fil_rouge/controllers/CategoryController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.backend_fil_rouge.models.Category;
 import com.example.backend_fil_rouge.services.CategoryService;
 
-@RestController
+@Controller
 @RequestMapping(path = "api/categories")
 public class CategoryController {
 
